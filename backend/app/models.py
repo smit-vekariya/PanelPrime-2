4d53981caa2973f4f16d08c0cafebd92ca8a1f42
smit <smitvekariya164@gmail.com>
2025-04-01 00:27:25 +0530

    remove task scheduler app from app

--- backend/app/models.py
+++ backend/app/models.py
@@ -18,22 +18,3 @@ class ContactUs(models.Model):
     class Meta:
         verbose_name_plural = "Contact Us"
         ordering = ['-created_on']
-
-
-class CommentQuestions(models.Model):
-    question = models.TextField()
-    created_on = models.DateTimeField(auto_now=True)
-    action_by = models.ForeignKey(BondUser, on_delete=models.PROTECT)
-
-    def __str__(self):
-        return self.question
-
-class CommentAnswer(models.Model):
-    questions = models.ForeignKey(CommentQuestions, related_name='answers', on_delete=models.CASCADE)
-    answer = models.TextField()
-    created_on = models.DateTimeField(auto_now=True)
-    action_by = models.ForeignKey(BondUser, on_delete=models.PROTECT)
-
-
-    def __str__(self):
-        return self.answer
