46fdf1221316d1f1804357e7bbbf71141ff914c6
smit <smitvekariya164@gmail.com>
2025-04-05 15:05:40 +0530

    add swagger

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'rest_framework',
+    'drf_yasg',
     'corsheaders',
     'crispy_forms',
     'crispy_bootstrap5',
