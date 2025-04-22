1de0dfa38ed2cee444d7341d37e807078bcaecb4
smitvekariya164 <smitvekariya164@gmail.com>
2025-04-22 10:04:53 +0000

    add new api for ai

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -8,7 +8,7 @@
   packages = [
     pkgs.openssh
     # pkgs.go
-    # pkgs.python311
+    pkgs.python311
     # pkgs.python311Packages.pip
     # pkgs.nodejs_20
     # pkgs.nodePackages.nodemon
--- backend/ai/urls.py
+++ backend/ai/urls.py
@@ -5,5 +5,5 @@ from .views import *
 app_name = "ai"
 
 urlpatterns = [
-    path('generate_mail/', GenerateMail.as_view(), name='generate_mail'),
+    path('ask_me_anything/', AskMeAnything.as_view(), name='ask_me_anything')
 ]
